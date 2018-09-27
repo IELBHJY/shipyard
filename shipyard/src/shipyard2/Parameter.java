bdab921395a49eaccf184b33863464211e1216aa
IELBH <1069494833@qq.com>
2018-09-27 14:14:59 +0800

    update parameter.java

--- shipyard/src/shipyard2/Parameter.java
+++ shipyard/src/shipyard2/Parameter.java
@@ -3,7 +3,7 @@ package shipyard2;
 public class Parameter {
     final static int N=20;
     final static int T=4;
-    static int superNum=0;//后边改成自己去获取
+    static int superNum=0;
     final static int size=20000;
     final static int ts_iteration=100;
     final static int tabuLength=30;
